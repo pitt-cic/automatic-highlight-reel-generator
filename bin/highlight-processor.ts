c0200537421bbe445a13bb08dcedff94470cea1d
morse-rowan <rowan.morse16@gmail.com>
2025-07-25 21:20:05 -0400

    First read me draft, changed name of stack

--- bin/highlight-processor.ts
+++ bin/highlight-processor.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { HighlightProcessorStack } from '../lib/highlight-processor-stack';
 
 const app = new cdk.App();
-new HighlightProcessorStack(app, 'HighlightProcessorStackV2', {
+new HighlightProcessorStack(app, 'HighlightProcessorStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
